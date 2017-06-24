00ad480d7165a3d4a753729bee5308fa5029bf62
Matt Arnold <matt@mattarnster.co.uk>
2017-06-24 01:40:13 +0100

    Add boilerplate for client/projects/times local storage stuff

---
+++ src/helpers/localStorageHelper.js
@@ -0,0 +1,30 @@
+
+export default class localStorageHelper {
+    /**
+     * Clients
+     * Projects
+     * Times
+     */
+
+
+    /**
+     * Clients
+     */
+    getClients() {}
+    putClient() {}
+    deleteClient() {}
+
+    /**
+     * Projects
+     */
+    getProjects() {}
+    putProject() {}
+    deleteProject() {}
+
+    /**
+     * Times
+     */
+    getTimes() {}
+    putTime() {}
+    deleteTime() {}
+}
