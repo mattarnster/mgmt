63a2cee25183ab7df51a41a1c21cd76afd45491f
Toby Osborne <toby.authenticstyle@gmail.com>
2017-06-24 23:20:54 +0100

    fixed random end close tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ export default class Footer extends PureComponent {
         <ul>
           <li><Link to="/"><i className="icon-user"></i><p>Clients</p></Link></li>
           <li>
-            {params[1] ? <Link to={ "/" + params[1] }><i className="icon-folder"></i>Projects</Link> : <span><i className="icon-folder"></i>Projects></span>}
+            {params[1] ? <Link to={ "/" + params[1] }><i className="icon-folder"></i>Projects</Link> : <span><i className="icon-folder"></i>Projects</span>}
           </li>
           <li>
             {params[1] && params[2] ? <Link to={"/"+params[1]+"/"+params[2] }><i className="icon-clock"></i>Timer</Link> : <span><i className="icon-clock"></i>Timer</span>}
