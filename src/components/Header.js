0c9deb02ab2efb66057b52aee47d2905918f9ddf
Matt Arnold <matt@mattarnster.co.uk>
2017-06-25 00:02:21 +0100

    Add sweetalert for settings.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,37 @@
 import React, { PureComponent } from 'react'
 
 export default class Header extends PureComponent {
+
+  showSettingsModal() {
+    window.swal({
+      title: 'Settings',
+      html:
+      '<div><label for="dark_mode">Dark mode <input id="dark_mode" type="checkbox"/></label></div>' +
+      '<div><label for="alerts">Alerts <input id="alerts" type="checkbox" /></label></div>' +
+      '<div><label for="alert_periods">Alert periods (mins)<input id="alert_periods" type="text" /></label></div>' +
+      '<div><button>Export JSON</button></div>' +
+      '<div><span>Import JSON</span></div>' +
+      '<div><input id="import" type="text" /></div>',
+      showCancelButton: true,
+      confirmButtonText: 'Submit',
+      showLoaderOnConfirm: true,
+      preConfirm: function () {
+        return new Promise(function (resolve, reject) {
+          // Do validation here
+          resolve([
+            document.getElementById('dark_mode').value,
+            document.getElementById('alerts').value,
+            document.getElementById('alert_periods').value,
+            document.getElementById('import').value
+          ])
+        })
+      },
+      allowOutsideClick: false
+    }).then(function (result) {
+      window.swal(JSON.stringify(result))
+    }).catch(window.swal.noop)
+  }
+
   render() {
     return (
       <header className="header">
@@ -12,7 +43,7 @@ export default class Header extends PureComponent {
           <span className="logo-letter four">T</span>
         </div>
         <div className="settings-container">
-          <span className="icon-cog"></span>
+          <span className="icon-cog" onClick={ this.showSettingsModal }></span>
         </div>
       </header>
     )
