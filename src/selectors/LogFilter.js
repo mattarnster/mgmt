b664faab4847b81647b8cdd79821008ba654c6e7
Toby Osborne <toby.authenticstyle@gmail.com>
2017-06-24 21:50:13 +0100

    Added the time to the returned log filter

--- src/selectors/LogFilter.js
+++ src/selectors/LogFilter.js
@@ -4,7 +4,7 @@ const logs = (state) => state.logs
 const projectId = (state, props) => props.match.params.projectId
 const duration = (state) => state.logFilterDuration;
 
-const name = (logId,duration) =>{
+const getName = (logId,duration) =>{
 
   logId = logId.replace(/[^0-9]/g, '')
   let date = new Date(parseFloat(logId))
@@ -17,12 +17,16 @@ const name = (logId,duration) =>{
       var  day = date.getDay(),
       diff = date.getDate() - day + (day === 0 ? -6:1),
       diffo = date.getDate() - day + (day === 6 ? 6:0)
-      return new Date(date.setDate(diff)).toLocaleDateString('en-GB',{day: 'numeric', month: 'short'})+"  to  "+ new Date(date.setDate(diffo)).toLocaleDateString('en-GB',{day: 'numeric', month: 'short'})+" "+date.getFullYear()
+      return new Date(date.setDate(diff)).toLocaleDateString('en-GB',{day: 'numeric', month: 'short'})+"  --  "+ new Date(date.setDate(diffo)).toLocaleDateString('en-GB',{day: 'numeric', month: 'short'})+" "+date.getFullYear()
 
     default:
       return date.toLocaleDateString('en-GB',{ year: 'numeric', month: 'long'})
   }
 
+}
+
+const getTime = (time) =>{
+  return new Date(time * 1000).toISOString().substr(11, 8)
 }
 
 
@@ -34,7 +38,7 @@ export const LogFilter = createSelector(
     return Object.assign({}, ...Object.keys(logs).map(k => (
 
       (logs[k].projectId === projectId &&
-       logs[k].type === duration ? {[k]: {name:name(k,duration)}} : null))
+       logs[k].type === duration ? {[k]: {name:getName(k,duration),time:getTime(logs[k].time)}} : null))
 
     ));
 
