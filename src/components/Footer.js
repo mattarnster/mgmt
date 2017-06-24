c7b3fc28a24201346c1c65d17884f07921cc8e1b
Toby Osborne <toby.authenticstyle@gmail.com>
2017-06-24 23:19:46 +0100

    Modified the footer to prevent undefined links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,17 +3,23 @@ import { Link } from 'react-router-dom'
 
 export default class Footer extends PureComponent {
   render() {
-    console.log(this.props.match.params[0].split("/"))
-    let projectsActiveState = (this.props.match.params[0].split("/")[1] ? true : false)
-    let timerActiveState = (this.props.match.params[0].split("/")[2] ? true : false)
-    let logActiveState = (this.props.match.params[0].split("/")[3] ? true : false)
+
+
+    let params = this.props.match.params[0].split("/")
+
     return (
       <footer className="footer">
         <ul>
-          <li><a><i className="icon-user"></i><p>Clients</p></a></li>
-          <li disabled={ projectsActiveState ? false : true }><Link to={ "/" + this.props.match.params[0].split("/")[1] }><i className="icon-folder"></i><p>Projects</p></Link></li>
-          <li disabled={ timerActiveState ? false : true }><Link to={ this.props.match.params[0].split("/")[1] + "/" + this.props.match.params[0].split("/")[2]}><i className="icon-clock"></i><p>Timer</p></Link></li>
-          <li disabled={ timerActiveState ? false : true }><Link to={ this.props.match.url + "/logs" }><i className="icon-clipboard"></i><p>Log</p></Link></li>
+          <li><Link to="/"><i className="icon-user"></i><p>Clients</p></Link></li>
+          <li>
+            {params[1] ? <Link to={ "/" + params[1] }><i className="icon-folder"></i>Projects</Link> : <span><i className="icon-folder"></i>Projects></span>}
+          </li>
+          <li>
+            {params[1] && params[2] ? <Link to={"/"+params[1]+"/"+params[2] }><i className="icon-clock"></i>Timer</Link> : <span><i className="icon-clock"></i>Timer</span>}
+          </li>
+          <li>
+            {params[1] && params[2] ? <Link to={"/"+params[1]+"/"+params[2]+"/logs" }><i className="icon-clipboard"></i>Logs</Link> : <span><i className="icon-clipboard"></i>Logs</span>}
+          </li>
         </ul>
       </footer>
     )
