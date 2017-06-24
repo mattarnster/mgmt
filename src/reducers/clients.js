fe76321f9b28d1ea4fa144a3e22e818d6793e72d
Matt Arnold <matt@mattarnster.co.uk>
2017-06-24 09:36:53 +0100

    Adding reselect

--- src/reducers/clients.js
+++ src/reducers/clients.js
@@ -17,15 +17,28 @@ const clients = (state = {}, action) => {
       return newClients
 
     case 'ADD_PROJECT_TO_CLIENT':
+      // Get the current client & projects array
+      let clientId = action.payload.clientId
+      let client = state[clientId]
+      let clientProjects = client.projects
       let newProject = {
         name: action.payload.project.projectName
       }
 
+      let mergedProjects = Object.assign(clientProjects, { [action.payload.project.projectId]: newProject })
 
-      //ls = new localStorageHelper()
-      //ls.putClients(newState)
+      //state[clientId].projects = mergedProjects
 
-      return state
+      var newState = Object.assign({}, state);
+
+      newState[clientId].projects = mergedProjects
+
+
+
+      ls = new localStorageHelper()
+      ls.putClients(newState)
+
+      return newState
 
     default:
       return state
