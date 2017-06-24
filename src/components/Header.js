ae13880e4c94a6adc20875e0e0400bcf1a1d5c45
authenticstyle <authenticstyle@George.local>
2017-06-24 06:06:30 +0100

    Added logo transition

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,19 @@ export default class Header extends PureComponent {
   render() {
     return (
       <header className="header">
-        <h1>Header</h1>
+        <div className="logo-container">
+          <span className="icon-clock"></span>
+          <span className="logo-letter one">M</span>
+          <span className="logo-letter two">G</span>
+          <span className="logo-letter three">M</span>
+          <span className="logo-letter four">T</span>
+        </div>
+        <div className="search-container">
+
+        </div>
+        <div className="settings-container">
+
+        </div>
       </header>
     )
   }
