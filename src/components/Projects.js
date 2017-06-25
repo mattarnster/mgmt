c9bc6f5d23d9d2bb95d82ff9d7b2383128953bf7
Matt Arnold <matt@mattarnster.co.uk>
2017-06-25 11:56:28 +0100

    Project editing

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,7 @@ import List from './List'
 import { connect } from 'react-redux'
 import { ProjectFilter } from '../selectors/ProjectFilter'
 
-import { addProject } from '../actions/actionCreators'
+import { addProject, editProject } from '../actions/actionCreators'
 
 
 
@@ -15,6 +15,8 @@ class Projects extends PureComponent {
     super()
 
     this.addProject = this.addProject.bind(this)
+    this.edit = this.edit.bind(this)
+
     document.title = 'Projects';
   }
 
@@ -49,6 +51,37 @@ class Projects extends PureComponent {
     })
   }
 
+  edit(projectId) {
+    var that = this;
+
+    window.swal({
+      title: 'Edit project: ' + this.props.projects[projectId].name,
+      input: 'text',
+      inputValue: this.props.projects[projectId].name,
+      showCancelButton: true,
+      confirmButtonColor: 'transparent',
+      cancelButtonColor: 'transparent',
+      showLoaderOnConfirm: false,
+      confirmButtonText: '<i class="icon icon-check-circle"></i>',
+      cancelButtonText: '<i class="icon icon-times-circle"></i>',
+      preConfirm: function (name) {
+        return new Promise(function (resolve, reject) {
+          if (name === that.props.projects[projectId].name) {
+            reject('Enter a different name or click cancel')
+          }
+          if (name === '') {
+            reject('Please enter a name')
+          } else {
+            resolve()
+          }
+        })
+      },
+      allowOutsideClick: false
+    }).then(function (name) {
+      that.props.dispatch(editProject(projectId, name))
+    })
+  }
+
   render() {
     return (
       <section className="card">
@@ -58,7 +91,7 @@ class Projects extends PureComponent {
         </header>
         <div className="card-body">
 
-          <List data={ this.props.projects } sPath={this.props.match.url+"/"}/>
+          <List data={ this.props.projects } edit={ this.edit } sPath={this.props.match.url+"/"}/>
 
         </div>
       </section>
