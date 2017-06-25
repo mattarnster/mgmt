9e29fbdfb860208a067451353c5c6ae11eed1783
Matt Arnold <matt@mattarnster.co.uk>
2017-06-25 17:03:29 +0100

    Settings caps

--- src/components/Header.js
+++ src/components/Header.js
@@ -44,11 +44,11 @@ class Header extends PureComponent {
     window.swal({
       title: 'Settings',
       html:
-      '<div class="settings-row"><p><span>Dark mode</span></p> <input id="dark_mode" class="styled-checkbox" type="checkbox" ' + (dm_status === true ? 'checked' : '') + '><label for="dark_mode"></label></div>' +
-      '<div class="settings-row"><p><span>Disable background animation</span></p> <input id="dis_anim" class="styled-checkbox" type="checkbox" ' + (anim_status === true ? 'checked' : '') + '><label for="dis_anim"></label></div>' +
-      '<div class="settings-row"><p><span>Disco mode</span></p> <input id="disco" class="styled-checkbox" type="checkbox" ' + (disco_status === true ? 'checked' : '') + '><label for="disco"></label></div>' +
+      '<div class="settings-row"><p><span>Dark Mode</span></p> <input id="dark_mode" class="styled-checkbox" type="checkbox" ' + (dm_status === true ? 'checked' : '') + '><label for="dark_mode"></label></div>' +
+      '<div class="settings-row"><p><span>Disable Background Animation</span></p> <input id="dis_anim" class="styled-checkbox" type="checkbox" ' + (anim_status === true ? 'checked' : '') + '><label for="dis_anim"></label></div>' +
+      '<div class="settings-row"><p><span>Disco Mode</span></p> <input id="disco" class="styled-checkbox" type="checkbox" ' + (disco_status === true ? 'checked' : '') + '><label for="disco"></label></div>' +
       '<div class="settings-row"><p><span>Alerts</span> </p><input id="alerts" class="styled-checkbox"  type="checkbox" ' + (al_status === true ? 'checked' : '' ) + '/><label for="alerts"></label></div>' +
-      '<div class="settings-row"><p><span>Alert periods <small>(mins)</small></span></p> <input id="alert_periods" min="1" step="1"   type="number" value="' + alp_status + '" /></div>',
+      '<div class="settings-row"><p><span>Alert Periods <small>(mins)</small></span></p> <input id="alert_periods" min="1" step="1"   type="number" value="' + alp_status + '" /></div>',
       width:400,
       padding:50,
       showCancelButton: true,
