345b0f46c4567b2db7a0dcfa5f7b85e11358d8a8
Matt Arnold <matthew.arnold@m-w.co.uk>
2017-07-28 09:59:00 +0100

    Dependency updates & removal of asset from footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react'
 import { Link } from 'react-router-dom'
-import ReactRiotLogo from '../assets/images/reactriot.png'
 
 export default class Footer extends PureComponent {
   render() {
